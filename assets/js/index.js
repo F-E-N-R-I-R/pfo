7d7ec3abf58cfe9411f27cd2b515439d01587ffa
Vladimir Zubkov <vladimirzubkov@Mac-Book-Pro-Vladimir.local>
2019-05-07 17:49:33 +0300

    Added last version for pfo

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,15 +1,30 @@
 $(function() {
-  const content = $('#pfo-content');
+    const Pfo = function() { this.init(); };
 
-  $(document)
-    .on('click', '#pfo-nvbar .btn-run', () => {
-      content
-        .removeClass('light')
-        .addClass('dark');
-    })
-    .on('click', '#pfo-nvbar .btn-group button', () => {
-      content
-        .removeClass('dark')
-        .addClass('light');
-    });
+    Pfo.prototype = {
+        init() {
+            this.container = $('#pfo');
+            this.navBar = new window.components.NavBar({ container: this.container });
+            this.sideBar = new window.components.SideBar({ container: this.container });
+            this.content = new window.components.Content({ container: this.container });
+            this._bindEvents();
+        },
+        _bindEvents() {
+            this.container
+                .on('sidebar:click:logo', () => {
+                    this.content.showInvite();
+                    this.navBar.hideButtons();
+                })
+                .on('sidebar:click:tab', (e, name) => {
+                    this.content.loadMapping(name);
+                    this.navBar.showButtons();
+                })
+                .on('navbar:click:html', () => this.content.onClickHtml())
+                .on('navbar:click:css', () => this.content.onClickCss())
+                .on('navbar:click:js', () => this.content.onClickJs())
+                .on('navbar:click:run', () => this.content.onClickRun());
+        },
+    };
+
+    new Pfo();
 });
